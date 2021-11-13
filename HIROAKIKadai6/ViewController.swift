8279c31c2114311400744cb1e519302f697ec4aa
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-13 18:04:53 +0900

    numberSliderをアウトレット接続

--- HIROAKIKadai6/ViewController.swift
+++ HIROAKIKadai6/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet private weak var themesLabel: UILabel!
+    @IBOutlet private weak var numberSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
