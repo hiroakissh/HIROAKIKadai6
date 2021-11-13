e472747fd5dd5896e1082c3b4e982fc14040fa9d
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-13 18:24:18 +0900

    変数の定義

--- HIROAKIKadai6/ViewController.swift
+++ HIROAKIKadai6/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     @IBOutlet private weak var themesLabel: UILabel!
     @IBOutlet private weak var numberSlider: UISlider!
 
+    private var themesNumber: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         themesLabel.text = String(arc4random_uniform(100))
